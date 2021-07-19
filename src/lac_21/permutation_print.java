37f9eede598c6a8417148e01001fd7586cebd897
iqbalahson <iqbal.ahson@yahoo.com>
2021-07-19 17:26:08 +0530

    iqbal

--- src/lac_21/permutation_print.java
+++ src/lac_21/permutation_print.java
@@ -7,7 +7,7 @@ public class permutation_print {
     }
     public static void  permutation(String ques, String ans ){
         if (ques.length()==0){
-            System.out.println(ans);
+//            System.out.println(ans);
             return;
         }
         for (int i = 0; i < ques.length()   ; i++) {
