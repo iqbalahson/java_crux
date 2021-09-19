79e7143b28020c3e0529dac27ce22acdd2545f9d
iqbal_ahson <iqbal.ahson@yahoo.com>
2021-09-20 00:04:54 +0530

    iqbal

---
+++ src/Lac5/Game_play.java
@@ -0,0 +1,36 @@
+package Lac5;
+
+import java.util.Scanner;
+
+public class Game_play {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt();
+        while (testcases-->0   ) {
+            int limit_Aysh = sc.nextInt();
+            int limit_Harshit = sc.nextInt();
+            play(limit_Aysh,limit_Harshit);
+
+        }
+    }
+    public static void  play    (int limit_Aush, int limit_Harshit){
+    int purch_phone = 1;
+    int Ayush_purchase = 0;
+    int Harshit_purchase = 0;
+    while(true){
+        Ayush_purchase = Ayush_purchase + purch_phone;
+        if (Ayush_purchase > limit_Aush) {
+            System.out.println("Ayush lost ");
+            break;
+        }
+        purch_phone++;
+        Harshit_purchase = Harshit_purchase+purch_phone;
+        if (Harshit_purchase>limit_Harshit)
+        {
+            System.out.println("Harshit lost"   );
+            break;
+        }
+
+    }
+    }
+}
---
+++ src/Lac5/Print_Sereis_Of_Given_Pattern.java
@@ -0,0 +1,24 @@
+package Lac5;
+
+import java.util.Scanner;
+
+public class Print_Sereis_Of_Given_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1  = sc.nextInt();
+        int num2 = sc.nextInt();
+        series(num1,num2);
+    }
+    public static void series(int num_pattern, int divisible_num){
+        int counter = 0;
+        int i = 1;
+        while (counter++<num_pattern){
+            int val = (3*i )+ 2 ;
+            if (val%divisible_num!=0){
+                System.out.println(val);
+
+            }
+            i++;
+        }
+    }
+}
